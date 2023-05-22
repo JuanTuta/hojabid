44e1c3808302f9c888aca2626fa34dba0ac12705
JuanTuta <est.juan.tuta@unimilitar.edu.co>
2023-05-22 12:42:47 -0500

    ss

--- conexion.php
+++ conexion.php
@@ -1,20 +1,19 @@
 <?php
-echo ("Vida F, Fsísima");
-$serverName = "serv1prub.database.windows.net"
-$connectionOptions= array(
+echo "Vida F, Fsísima";
+$serverName = "serv1prub.database.windows.net";
+$connectionOptions = array(
     "Database" => "PPagina",
     "Authentication" => "ActiveDirectoryInteractive",
-    "Uid" => "est.juan.tuta@unimilitar.edu.co",
+    "UID" => "est.juan.tuta@unimilitar.edu.co",
     "PWD" => "Jp12021866*"
 );
 
-## Establece conexión
+// Establece conexión
 $conn = sqlsrv_connect($serverName, $connectionOptions);
 if ($conn === false) {
-    die(formatErrors(sqlsrv_errors()));
-    echo ("Vida F, Fsísima");
-}
-else {
-    //Echo ("TUMBA LA CASAAAA MAMIII");
+    die(print_r(sqlsrv_errors(), true));
+    echo "Vida F, Fsísima";
+} else {
+    echo "TUMBA LA CASAAAA MAMIII";
 }
 ?>
