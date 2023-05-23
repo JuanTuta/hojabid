9a8f56d225fcd17b4aa6fd7803d4da3985329613
DavidDiazS25 <91351023+DavidDiazS25@users.noreply.github.com>
2023-05-23 15:08:07 -0500

    Header Empresaaaaaa

---
+++ headerEmpresa.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Mi página web</title>
+  <link rel="stylesheet" href="css/css_Header.css">
+</head>
+<body>
+  <header>
+    <div class="barra_de_inicio">
+        <div class="Imagen">
+        <img src="imgs/Empregod.png" alt="Logo de la empresa">
+        </div>
+        <div class="Panel_gris">
+        <p class="cerda">¡Hola, [Cerdo capitalista]!</p>
+        </div>
+        <div class="logout">
+        <button class="ciérrate">Cerrar sesión</button>
+        </div>
+    </div>
+  </header>
+</body>
+</html>
