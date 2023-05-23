a02319daa7b22d496873470d186ec0b0d1bd693a
JuanTuta <est.juan.tuta@unimilitar.edu.co>
2023-05-23 08:14:13 -0500

    Update procesar_registro.php

--- procesar_registro.php
+++ procesar_registro.php
@@ -7,53 +7,58 @@ $connectionOptions = array(
     "Authentication" => "ActiveDirectoryPassword"
 );
 
-// Obtener los valores enviados por el formulario
-$nombre = $_POST['nombre'];
-$direccion = $_POST['direccion'];
-$telefono = $_POST['numero'];
-$ubicacion = $_POST['ubicacion'];
-$salario = $_POST['salario'];
-$hv = $_FILES['hv']['tmp_name'];
-$video = $_POST['video'];
-$usuario = $_POST['usuario'];
-$contrasena = $_POST['contrasena'];
+// Verificar si se enviaron los datos del formulario
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    // Obtener los valores del formulario
+    $nombre = $_POST["nombre"];
+    $direccion = $_POST["direccion"];
+    $telefono = $_POST["telefono"];
+    $ubicacion = $_POST["ubicacion"];
+    $salario = $_POST["salario"];
+    $hv = $_FILES["hv"]["tmp_name"];
+    $video = $_POST["video"];
+    $usuario = $_POST["usuario"];
+    $contrasena = $_POST["contrasena"];
 
-try {
-    $conn = sqlsrv_connect($serverName, $connectionOptions);
-    if ($conn === false) {
-        die(print_r(sqlsrv_errors(), true));
+    try {
+        $conn = sqlsrv_connect($serverName, $connectionOptions);
+        if ($conn === false) {
+            die(print_r(sqlsrv_errors(), true));
+            echo "Error de conexión";
+        } else {
+            // Insertar en la tabla DESEMPLEADO
+            $sqlDesempleado = "INSERT INTO DESEMPLEADO (NOMBRE, DIRECCION, TELEFONO, id_UBICACION, SALARIO, HV, VIDEO)
+                               VALUES (?, ?, ?, ?, ?, ?, ?)";
+            $paramsDesempleado = array($nombre, $direccion, $telefono, $ubicacion, $salario, file_get_contents($hv), $video);
+            $stmtDesempleado = sqlsrv_query($conn, $sqlDesempleado, $paramsDesempleado);
+            if ($stmtDesempleado === false) {
+                die(print_r(sqlsrv_errors(), true));
+                echo "Error al insertar en la tabla DESEMPLEADO";
+            }
+
+            // Obtener el ID_DESEMPLEADO generado automáticamente
+            $idDesempleado = sqlsrv_fetch_array(sqlsrv_query($conn, "SELECT SCOPE_IDENTITY()"));
+
+            // Insertar en la tabla usuarioSGod
+            $sqlUsuario = "INSERT INTO usuarioSGod (nombreUsuario, contraseña) VALUES (?, ?)";
+            $paramsUsuario = array($usuario, $contrasena);
+            $stmtUsuario = sqlsrv_query($conn, $sqlUsuario, $paramsUsuario);
+            if ($stmtUsuario === false) {
+                die(print_r(sqlsrv_errors(), true));
+                echo "Error al insertar en la tabla usuarioSGod";
+            }
+
+            // Cerrar la conexión a la base de datos
+            sqlsrv_close($conn);
+
+            echo "Datos insertados correctamente";
+        }
+    } catch (Exception $e) {
+        echo "Error: " . $e->getMessage();
     }
-
-    // Insertar en la tabla DESEMPLEADO
-    $sqlDesempleado = "INSERT INTO DESEMPLEADO (NOMBRE, DIRECCION, TELEFONO, id_UBICACION, SALARIO, HV, VIDEO)
-                       VALUES (?, ?, ?, ?, ?, ?, ?)";
-    $paramsDesempleado = array($nombre, $direccion, $telefono, $ubicacion, $salario, file_get_contents($hv), $video);
-    $stmtDesempleado = sqlsrv_query($conn, $sqlDesempleado, $paramsDesempleado);
-    if ($stmtDesempleado === false) {
-        die(print_r(sqlsrv_errors(), true));
-    }
-
-    // Obtener el ID_DESEMPLEADO generado automáticamente
-    $idDesempleado = sqlsrv_fetch_array(sqlsrv_query($conn, "SELECT @@IDENTITY"));
-
-    // Insertar en la tabla usuarioSGod
-    $sqlUsuario = "INSERT INTO usuarioSGod (nombreUsuario, contraseña) VALUES (?, ?)";
-    $paramsUsuario = array($usuario, $contrasena);
-    $stmtUsuario = sqlsrv_query($conn, $sqlUsuario, $paramsUsuario);
-    if ($stmtUsuario === false) {
-        die(print_r(sqlsrv_errors(), true));
-    }
-
-    // Cerrar la conexión a la base de datos
-    sqlsrv_close($conn);
-
-    // Redireccionar a una página de éxito o mostrar un mensaje de éxito
-    header("funco");
-    exit();
-} catch (Exception $e) {
-    echo "Error: " . $e->getMessage();
 }
 ?>
+
 
 
 
