a0a2f0603a5ab13c38f13e3dfbfdc693dd6d3a47
JuanTuta <est.juan.tuta@unimilitar.edu.co>
2023-05-23 17:32:38 -0500

    Update headerEmpresa.php

--- headerEmpresa.php
+++ headerEmpresa.php
@@ -3,6 +3,7 @@
 <head>
   <title>Mi página web</title>
   <link rel="stylesheet" href="css/css_Header.css">
+  <link rel="stylesheet" href="css/log_in.css">
 </head>
 <body>
   <header>
@@ -18,5 +19,23 @@
         </div>
     </div>
   </header>
+  <body>
+        <div id="container">
+            <div class="info">
+              <h1>Agencias de Empleos Bogotá</h1>
+              <span>Made with LOVE, SWEAT AND BLOOD <i class="fa fa-heart"></i></span>
+            </div>
+            <div class="form">
+              <div class="thumbnail">
+                <img src="https://cdn-icons-png.flaticon.com/512/4832/4832900.png" />
+              </div>
+              <form class="login-form">
+                <a class="button2" href="https://emprevib.azurewebsites.net/crearcuentaU.php">Usuario</a>
+                <a class="button2" href="https://emprevib.azurewebsites.net/crearcuentaE.php">Empresa</a>
+                <p class="message">Admin? <a href="#">Click aqui</a></p>
+              </form>
+            </div>
+          </div>
+    </body>
 </body>
 </html>
