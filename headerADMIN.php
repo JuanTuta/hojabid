758d27ad4bec2cbb86c6e4961d4983f0a9298677
DavidDiazS25 <91351023+DavidDiazS25@users.noreply.github.com>
2023-05-23 18:07:49 -0500

    Push

---
+++ headerADMIN.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Mi página web</title>
+    <link rel="stylesheet" href="css/css_Header.css">
+    <link rel="stylesheet" href="css/log_in.css">
+  </head>
+  <body>
+    <header>
+      <div class="barra_de_inicio">
+          <div>
+          <img  class="Imagen" src="imgs/admin.jpg" alt="Ícono distintivo empresa">
+          </div>
+          <div class="Panel_gris">
+          <p class="cerda">¡Hola, ADMINAZOOO!!!</p>
+          </div>
+          <div>
+          <a class="logout" href="xd">Cerrar sesión</a>
+          </div>
+      </div>
+    </header>
+  </body>
+</html>
