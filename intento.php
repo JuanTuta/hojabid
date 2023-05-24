c61f766fca684fb8b8a59558142d0485e6421ea0
JuanTuta <est.juan.tuta@unimilitar.edu.co>
2023-05-24 09:54:02 -0500

    Update intento.php

--- intento.php
+++ intento.php
@@ -4,48 +4,54 @@ session_start();
 $serverName = "serv1prub.database.windows.net";
 $connectionOptions = array(
     "Database" => "PPagina",
-    "UID" => "est.juan.tuta@unimilitar.edu.co",
+    "Uid" => "est.juan.tuta@unimilitar.edu.co",
     "PWD" => "Jp12021866*",
     "Authentication" => "ActiveDirectoryPassword"
 );
 
 // Establecer la conexión con el servidor y realizar la consulta
 try {
-    $conn = new mysqli($serverName, $connectionOptions["UID"], $connectionOptions["PWD"], $connectionOptions["Database"]);
-
-    if ($conn->connect_error) {
-        die("Error de conexión: " . $conn->connect_error);
+    $conn = sqlsrv_connect($serverName, $connectionOptions);
+    
+    if ($conn === false) {
+        die(print_r(sqlsrv_errors(), true));
     }
+
+    $sql = "SELECT * FROM DESEMPLEADO";
+    $result = sqlsrv_query($conn, $sql);
+
+    if ($result === false) {
+        die(print_r(sqlsrv_errors(), true));
+    }
+
+    if (sqlsrv_has_rows($result)) {
+        while ($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)) {
+            $nombreDesempleado = $row["DESEMPLEADO"];
+            $ubicacionId = $row["ID_UBICACION"];
+
+            // Obtener el nombre de la ubicación de la tabla UBICACION
+            $sqlUbicacion = "SELECT CIUDAD FROM UBICACION WHERE ID_UBICACION = $ubicacionId";
+            $resultUbicacion = sqlsrv_query($conn, $sqlUbicacion);
+            $rowUbicacion = sqlsrv_fetch_array($resultUbicacion, SQLSRV_FETCH_ASSOC);
+            $nombreUbicacion = $rowUbicacion["CIUDAD"];
+
+            // Generar el div con los datos obtenidos
+            echo '<div class="Contenedor-HV-2">
+                    <div class="mini-blanco-2">
+                        <h3 class="Encabezadín-2">' . $nombreDesempleado . ' (' . $nombreUbicacion . ')</h3>
+                    </div>
+                    <a class="archivoHV-2">Descargala ahora</a>
+                </div>';
+        }
+    } else {
+        echo "No se encontraron registros en la tabla DESEMPLEADO.";
+    }
+
+    sqlsrv_free_stmt($result);
+    sqlsrv_close($conn);
 } catch (Exception $e) {
     echo "Error: " . $e->getMessage();
 }
-
-$sql = "SELECT * FROM DESEMPLEADO";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        $nombreDesempleado = $row["DESEMPLEADO"];
-        $ubicacionId = $row["ID_UBICACION"];
-
-        // Obtener el nombre de la ubicación de la tabla UBICACION
-        $sqlUbicacion = "SELECT CIUDAD FROM UBICACION WHERE ID_UBICACION = $ubicacionId";
-        $resultUbicacion = $conn->query($sqlUbicacion);
-        $rowUbicacion = $resultUbicacion->fetch_assoc();
-        $nombreUbicacion = $rowUbicacion["CIUDAD"];
-
-        // Generar el div con los datos obtenidos
-        echo '<div class="Contenedor-HV-2">
-                <div class="mini-blanco-2">
-                    <h3 class="Encabezadín-2">' . $nombreDesempleado . ' (' . $nombreUbicacion . ')</h3>
-                </div>
-                <a class="archivoHV-2">Descargala ahora</a>
-            </div>';
-    }
-} else {
-    echo "No se encontraron registros en la tabla DESEMPLEADO.";
-}
-
-$conn->close();
 ?>
+
 
