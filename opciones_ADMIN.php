2fd0069e638fcd1514ad7fb3bab7a07aa974dcfa
DavidDiazS25 <91351023+DavidDiazS25@users.noreply.github.com>
2023-05-23 21:58:22 -0500

    dasdsdad

---
+++ opciones_ADMIN.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Mi página web</title>
+    <link rel="stylesheet" href="css/opciones.css">
+  </head>
+  <body>
+    <?php include 'headerADMIN.php'; ?>
+    <div id="container">
+      <div class="Blancou">
+        <h1>¿Qué quieres hacer?</h1>
+        <div class="botones-container">
+          <div class="BOTONAZO">
+            <img src="imgs/reanudar.png" class="imagen1" />
+          </div>
+          <div class="BOTONAZO2">
+            <img src="imgs/cv.png" class="imagen2" />
+          </div>
+    </div>
+    </div>
+        <img src="imgs/admin.png" class="imTroll" />
+      </div>
+  </body>
+</html>
