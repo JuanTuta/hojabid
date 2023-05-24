4553f2d3a9ad270a55ef83f32abcad05ec8d8c7d
DavidDiazS25 <91351023+DavidDiazS25@users.noreply.github.com>
2023-05-23 23:33:57 -0500

    dsadasdads

---
+++ consultaHV-empresa.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Mi página web</title>
+    <link rel="stylesheet" href="css/opciones.css">
+  </head>
+  <body>
+    <?php include 'headerEmpresa.php'; ?>
+    <div class="container-rojo">
+        <div class="container-blanco">
+            <h1>HV disponibles</h1>
+            <div class="Contenedor-HV">
+                    <div class="mini-blanco">
+                        <h3 class="Encabezadín-1">TutiGod (Bogotá D.C.)</h3>
+                    </div>
+                    <a class="archivoHV-1">Descargala ahora</a>
+            </div>
+
+            <div class="Contenedor-HV-2">
+                    <div class="mini-blanco-2">
+                        <h3 class="Encabezadín-2">TutiGod (Bogotá D.C.)</h3>
+                    </div>
+                    <a class="archivoHV-2">Descargala ahora</a>
+            </div>
+
+        </div>
+    </div>
+
+
+
+
+  </body>
+</html>
