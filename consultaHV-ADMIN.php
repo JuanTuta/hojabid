02ef723cef912bc66691590d3c0253f177c084d4
JuanTuta <est.juan.tuta@unimilitar.edu.co>
2023-05-24 11:54:43 -0500

    Create consultaHV-ADMIN.php

---
+++ consultaHV-ADMIN.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Mi página web</title>
+    <link rel="stylesheet" href="css/css_consultaHV.css">
+  </head>
+  <body>
+    <?php include 'headerADMIN.php'; ?>
+    <div class="container-rojo">
+        <div class="container-blanco">
+            <h1>HV disponibles</h1>
+            <?php include 'intento.php'; ?>
+        </div>
+    </div>
+
+
+
+
+  </body>
+</html>
